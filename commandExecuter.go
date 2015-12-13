cbfea2fa14befd2d67f98d55b4174327353621fc
Stefan Knott <stkn5801@colorado.edu>
2015-12-13 11:42:31 -0700

    report output data via channels

--- commandExecuter.go
+++ commandExecuter.go
@@ -15,7 +15,8 @@ import (
 var wg sync.WaitGroup
 
 type output struct{
-	filename, word, count string
+	filename, word string 
+	count int
 	dur time.Duration
 }
 
@@ -27,7 +28,7 @@ func checkSum(filename string){
 }
 
 //NOTE: words connected by a hyphen are counted as one whole word
-func wordCount(filename string){
+func wordCount(filename string, wcChan chan output){
 	startTime := time.Now()
 	defer wg.Done()
 	file, err := os.Open(filename)
@@ -43,11 +44,12 @@ func wordCount(filename string){
 		wc += len(words)
 	}
 	dur := time.Since(startTime)
-	fmt.Printf("WORDCOUNT, %s, %d, %v\n",filename, wc, dur)
+	wcChan <- output{filename:filename, count:wc, dur:dur, word:""}
+	//fmt.Printf("WORDCOUNT, %s, %d, %v\n",filename, wc, dur)
 }
 
 //Checks the frequency of occurance of word within a file with a given filename
-func wordFreq(filename string , word string){
+func wordFreq(filename string , word string, wfChan chan output){
 	startTime := time.Now()
 	defer wg.Done()
 	file, err := os.Open(filename)
@@ -68,14 +70,47 @@ func wordFreq(filename string , word string){
 	}
 
 	dur := time.Since(startTime)
-	fmt.Printf("WORDFREQ, %s, %s, %d, %v\n", filename, word, wc, dur)
+	wfChan <- output{filename:filename, word:word, count:wc, dur:dur}
+	//fmt.Printf("WORDFREQ, %s, %s, %d, %v\n", filename, word, wc, dur)
+}
+
+//Spin up routines collecting output data from given channels, close channels when done extracting data
+func collector(wcChan chan output, wfChan chan output, csChan chan output){
+	go func(){
+		defer close(wcChan)
+		for res := range wcChan{
+			fmt.Printf("WORDCOUNT, %s, %d, %v\n", res.filename, res.count, res.dur)
+		}
+	}()
+
+	go func(){
+		defer close(wfChan)
+		for res := range wfChan{
+			fmt.Printf("WORDFREQ, %s, %s, %d, %v\n", res.filename, res.word, res.count, res.dur)
+		}
+	}()
+
+	/*go func(){
+		defer close(csChan)
+		for res := range csChan{
+			fmt.Println(res)
+			//fmt.Println("CHECKSUM, %s, %s, %d, %v\n", res.filename, res.word, res.count, res.dur)
+		}
+	}()*/
+	csChan = nil
 }
 
 //Produce work request to be done using csv, trimming white space from values
-func cmdReader(filename string){
+func cmdReader(cmdFile string){
 	defer wg.Done()
-	cmds, _ := os.Open(filename)
+	cmds, _ := os.Open(cmdFile)
 	defer cmds.Close()
+
+	wcChan := make(chan output)
+	wfChan := make(chan output)
+	csChan := make(chan output)
+
+	go collector(wcChan, wfChan, csChan)
 
 	r := csv.NewReader(bufio.NewReader(cmds))
 	for{
@@ -91,11 +126,11 @@ func cmdReader(filename string){
 			go checkSum(arg1)
 		case "WORDCOUNT":
 			wg.Add(1)
-			go wordCount(arg1)
+			go wordCount(arg1, wcChan)
 		case "WORDFREQ":
 			wg.Add(1)
 			arg2 := strings.TrimSpace(record[2])
-			go wordFreq(arg1, arg2)
+			go wordFreq(arg1, arg2, wfChan)
 		default:
 			fmt.Println("Invalid command: ", cmd)
 		}
@@ -103,8 +138,8 @@ func cmdReader(filename string){
 }
 
 func main(){
-	filename := os.Args[1]
+	cmdFile := os.Args[1]
 	wg.Add(1)
-	go cmdReader(filename)
+	go cmdReader(cmdFile)
 	wg.Wait()
 }
